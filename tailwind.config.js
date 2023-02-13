850faa34a45aec9a9fae96cb37524caf1b74ce07
Kartik Chaturvedi <kchaturvedi@users.noreply.github.com>
2023-02-13 12:54:23 -0500

    styling fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      gold: colors.amber[400],
-      green: colors.green[400],
-      blue: colors.blue[400],
-      red: colors.red[400],
-      gray: colors.gray[400],
+      gold: colors.amber,
+      green: colors.green,
+      blue: colors.blue,
+      red: colors.red,
+      gray: colors.gray,
     },
     extend: {},
   },
