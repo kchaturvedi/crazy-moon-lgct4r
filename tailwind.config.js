44cae90fd2ac7ab72aa1a11f24cad3be3dea2863
Kartik Chaturvedi <kchaturvedi@users.noreply.github.com>
2023-02-13 12:50:19 -0500

    styling fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      gold: colors.amber,
-      green: colors.green,
-      blue: colors.blue,
-      red: colors.red,
-      gray: colors.gray,
+      gold: colors.amber[400],
+      green: colors.green[400],
+      blue: colors.blue[400],
+      red: colors.red[400],
+      gray: colors.gray[400],
     },
     extend: {},
   },
   plugins: [],
-};
+}
