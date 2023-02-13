37d47b65a5594021aa554c6802ebb60af28733a3
Kartik Chaturvedi <kchaturvedi@users.noreply.github.com>
2023-02-13 12:50:39 -0500

    styling fix

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 
 const Card = ({ route }) => (
-  <div className={clsx(`flex flex-col bg-white border border-t-4 p-4 rounded-lg border-t-${route.color}-400`)}>
+  <div className={clsx(`flex flex-col bg-white border border-t-4 p-4 rounded-lg border-t-${route.color}`)}>
     <div className='flex justify-between'>
       <span className='text-gray-500'>{route.WAITING_TIME === 'Arriving' ? `Boarding at ${route.STATION}` : `${route.WAITING_TIME} to ${route.STATION}`}</span>
       <span className='text-gray-500'>
