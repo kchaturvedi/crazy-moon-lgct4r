e3d9b73bb6c886c6ad2ce2277097ad9a4dfcd6df
Kartik Chaturvedi <kchaturvedi@users.noreply.github.com>
2023-02-13 12:45:56 -0500

    styling fix

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,29 +1,18 @@
 import clsx from "clsx";
 
 const Card = ({ route }) => (
-  <div
-    className={clsx(
-      "flex flex-col bg-white border border-t-4 p-4 rounded-lg",
-      `border-t-${route.color}-400`
-    )}
-  >
-    <div className="flex justify-between">
-      <span className="text-gray-500">
-        {route.WAITING_TIME === "Arriving"
-          ? `Boarding at ${route.STATION}`
-          : `${route.WAITING_TIME} to ${route.STATION}`}
-      </span>
-      <span className="text-gray-500">
+  <div className={clsx(`flex flex-col bg-white border border-t-4 p-4 rounded-lg border-t-${route.color}-400`)}>
+    <div className='flex justify-between'>
+      <span className='text-gray-500'>{route.WAITING_TIME === 'Arriving' ? `Boarding at ${route.STATION}` : `${route.WAITING_TIME} to ${route.STATION}`}</span>
+      <span className='text-gray-500'>
         {route.LINE} Line — {route.DIRECTION}
       </span>
     </div>
-    <div className="flex justify-between">
-      <span className="text-lg font-semibold">ETA {route.NEXT_ARR}</span>
-      <span className="text-lg font-semibold">
-        Destination: {route.DESTINATION}
-      </span>
+    <div className='flex justify-between'>
+      <span className='text-lg font-semibold'>ETA {route.NEXT_ARR}</span>
+      <span className='text-lg font-semibold'>Destination: {route.DESTINATION}</span>
     </div>
   </div>
-);
+)
 
 export default Card;
